a88f3705dc1cabcc7c4184e12901e300d9abf205
aman1699 <amankumar9735@gmail.com>
2020-05-04 17:40:55 +0530

    bgbgf

--- helloWorldFX/src/sample/Controller.java
+++ helloWorldFX/src/sample/Controller.java
@@ -3,17 +3,22 @@ package sample;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
@@ -26,9 +31,16 @@ public class Controller<La> implements Initializable {
 
     @FXML
     private Pane visible;
+    private Object Node;
 
-    public void Clicked(ActionEvent event){
+    public void Clicked(ActionEvent event) throws IOException {
+
         System.out.println("button clicked");
+        Parent home_page_parent= FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        Scene home_page_scene=new Scene(home_page_parent);
+        Stage app_Stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_Stage.setScene(home_page_scene);
+        app_Stage.show();
     }
     /**
      * Initializes the controller class.
